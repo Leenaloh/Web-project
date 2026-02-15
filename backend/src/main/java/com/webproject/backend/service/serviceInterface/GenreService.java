c8bbb233d3d8e45545dc8134cde95fbf35e76b8e
Munalna <mualnasser3@gmail.com>
2026-02-15 09:50:42 +0300

    Revert "Roaa phase2"

--- backend/src/main/java/com/webproject/backend/service/serviceInterface/GenreService.java
+++
@@ -1,8 +0,0 @@
-package com.webproject.backend.service.serviceInterface;
-
-import com.webproject.backend.model.Genre;
-import java.util.List;
-
-public interface GenreService {
-    List<Genre> getAllGenres();
-}
